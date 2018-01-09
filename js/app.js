ead0911a990e2d41b12a67f4fe8ad98edb32ac04
Sue Chaplain <sue_chaplain@uk.ibm.com>
2018-01-09 16:55:23 +0000

    Update Copyright headers, fix V8 build dir names
    Update copyright headers for 2017, 2018 and
    replace SPDX-License-Identifier line as per
    openj9 source. Issue #55
    
    OpenJDK 8 build image directories are not the
    same as V9. Fix as per issue #65
    
    Fixes: #55
    Fixes: #56
    
    Signed-off-by: Sue Chaplain <sue_chaplain@uk.ibm.com>

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2017 IBM Corp. and others
+Copyright (c) 2017, 2018 IBM Corp. and others
 
 This program and the accompanying materials are made available under the terms of the Eclipse Public License 2.0 which accompanies this distribution and is available at http://eclipse.org/legal/epl-2.0 or the Apache License, Version 2.0 which accompanies this distribution and is available at https://www.apache.org/licenses/LICENSE-2.0. 
 
@@ -8,7 +8,7 @@ This Source Code may also be made available under the following Secondary Licens
 [1] https://www.gnu.org/software/classpath/license.html  
 [2] http://openjdk.java.net/legal/assembly-exception.html 
 
-SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
  */
 var readMoreButton = document.getElementById('read-more-button');
 var readMoreButtonIcon = document.getElementById('read-more-button-icon');
