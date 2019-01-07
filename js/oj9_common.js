44030a59c2b9cae2660c1ebc3731d9cbf0e25a10
Adam Brousseau <adam.brousseau88@gmail.com>
2019-01-07 09:05:55 -0500

    Add Docs link to navigation menu
    - This adds a Docs link to the upperleft most
      drop down navigation menu.
    - Previously the only Docs link on the main page
      was somewhat hard to find
    - Fix some minor formatting
    
    Signed-off-by: Adam Brousseau <adam.brousseau88@gmail.com>

--- js/oj9_common.js
+++ js/oj9_common.js
@@ -5,14 +5,14 @@ This program and the accompanying materials are made available under the terms o
 
 This Source Code may also be made available under the following Secondary Licenses when the conditions for such availability set forth in the Eclipse Public License, v. 2.0 are satisfied: GNU General Public License, version 2 with the GNU Classpath Exception [1] and GNU General Public License, version 2 with the OpenJDK Assembly Exception [2]. 
 
-[1] https://www.gnu.org/software/classpath/license.html  
-[2] http://openjdk.java.net/legal/assembly-exception.html 
+[1] https://www.gnu.org/software/classpath/license.html
+[2] http://openjdk.java.net/legal/assembly-exception.html
 
 SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
 
 The project website pages cannot be redistributed
  */
- 
+
  function test(url) {
 //this removes the anchor at the end, if there is one
 url = url.substring(0, (url.indexOf("#") == -1) ? url.length : url.indexOf("#"));
@@ -40,21 +40,23 @@ function navigation(url) {
 
   if(url == "oj9_whatsnew"   ) document.write("<p><i class='fa fa-chevron-circle-right f_mini' aria-hidden='true'></i>What's new?</p>");
   else                         document.write("<p><i class='fa fa-chevron-circle-right f_mini' aria-hidden='true'></i><a class='nav-item' href='oj9_whatsnew.html'>What's new?</a></p>");
-  
   if(url == "oj9_build"      ) document.write("<p><i class='fa fa-chevron-circle-right f_mini' aria-hidden='true'></i>Build</p>");
   else                         document.write("<p><i class='fa fa-chevron-circle-right f_mini' aria-hidden='true'></i><a class='nav-item' href='oj9_build.html'>Build</a></p>");
-  	
+
   if(url == "oj9_performance") document.write("<p><i class='fa fa-chevron-circle-right f_mini' aria-hidden='true'></i>Performance</p>");
   else                         document.write("<p><i class='fa fa-chevron-circle-right f_mini' aria-hidden='true'></i><a class='nav-item' href='oj9_performance.html'>Performance</a></p>");
-  	
+
   if(url == "oj9_resources"  ) document.write("<p><i class='fa fa-chevron-circle-right f_mini' aria-hidden='true'></i>Resources</p>");
   else                         document.write("<p><i class='fa fa-chevron-circle-right f_mini' aria-hidden='true'></i><a class='nav-item' href='oj9_resources.html'>Resources</a></p>");
- 
+
   if(url == "https://blog.openj9.org/"  ) document.write("<p><i class='fa fa-chevron-circle-right f_mini' aria-hidden='true'></i>Blog</p>");
   else                         document.write("<p><i class='fa fa-chevron-circle-right f_mini' aria-hidden='true'></i><a class='nav-item' href='https://blog.openj9.org/' target='_blank'>Blog</a></p>");
-  
+
   if(url == "oj9_faq"        ) document.write("<p><i class='fa fa-chevron-circle-right f_mini' aria-hidden='true'></i>FAQ</p>");
   else                         document.write("<p><i class='fa fa-chevron-circle-right f_mini' aria-hidden='true'></i><a class='nav-item' href='oj9_faq.html'>FAQ</a></p>");
+
+  if(url == "https://www.eclipse.org/openj9/docs/") document.write("<p><i class='fa fa-chevron-circle-right f_mini' aria-hidden='true'></i>Docs</p>");
+  else                         document.write("<p><i class='fa fa-chevron-circle-right f_mini' aria-hidden='true'></i><a class='nav-item' href='https://www.eclipse.org/openj9/docs/' target='_blank'>Docs</a></p>");
 
   document.write("</div>");
   document.write("</div>");
@@ -148,5 +150,3 @@ function tabs(id) {
 
   selectTab(activeTab || tabs[0]);
 }
-
-
