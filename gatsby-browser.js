27aa5ddd21ea4aa773a58cf208e234c039c2fbc5
Bazif Khan <bazif.khan@ibm.com>
2020-01-27 10:17:15 +0000

    Responsive About Page
    Signed-off-by: Bazif Khan <bazif.khan@ibm.com>

--- gatsby-browser.js
+++
@@ -1,3 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './src/styles/main.css';
-import '@fortawesome/fontawesome-free/css/all.css'
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,95 +1,139 @@
-import React from "react"
+import React from "react";
+import Layout from "../components/layout";
 
-import Layout from "../components/layout"
-import { Link } from "gatsby";
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import githubIcon from "../images/github_icon.svg";
-import twitterIcon from "../images/twitter_icon.svg";
-import slackIcon from "../images/slack_icon.svg";
-import Tile from "../components/tile";
+/** @jsx jsx */
+import { jsx, Styled } from "theme-ui";
 
-const AboutPage = () => (
-    <Layout>
-        <Navbar className="text-light bg-dark desktop-only">
-            <Nav>
-                <Nav.Link className="nav-link" href="/">OpenJ9</Nav.Link>
-            </Nav>
-            <Nav className="ml-auto">
-                <Nav.Link className="nav-link" href="/about">About</Nav.Link>
-                <Nav.Link className="nav-link" href="#link">Docs</Nav.Link>
-                <Nav.Link className="nav-link" href="#link">News</Nav.Link>
-                <Nav.Link href="#"><img className="navbar-img" src={ githubIcon } alt=""></img></Nav.Link>
-                <Nav.Link href="#"><img id="twitter-nav" className="navbar-img" src={ twitterIcon } alt=""></img></Nav.Link>
-                <Nav.Link href="#"><img className="navbar-img" src={ slackIcon } alt=""></img></Nav.Link>
-            </Nav>
-        </Navbar>
+import circlePatternBig from "../images/circle-pattern-big.svg";
+import Card from "../components/card";
+import Button from "../components/button";
 
-        <section className="landing-section light-gray-background">
-            <div className="desktop-only">
-                <h2 className="light-gray-section-heading">More About OpenJ9</h2>
-            </div>
-            <div className="card desktop-only ">
-                <div className="container">
-                    <div className="row">
-                        <div className="col">
-                            <p className="light-gray-card-text">Interested in contributing to OpenJ9? Check out the contributor guide in our Github repository.</p>
-                            <Link to="#" className="btn btn-primary light-gray-section-button" role="button">Call to Action</Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="container mobile-only">
-                <h2 className="light-gray-section-heading-mobile">More About OpenJ9</h2>
-                <div className="row">
-                    <p className="light-gray-card-text">Interested in contributing to OpenJ9? Check out the contributor guide in our Github repository.</p>
-                    <Link to="#" className="btn btn-primary light-gray-section-button" role="button">OpenJ9 GitHub</Link>
-                </div>
-            </div>
-        </section>
-
-        <section className="bg-gray-img">
-            <div className="our-sponsors row">
-                <Tile heading="Sponsor Name" desc="Sponsorship and support via" className="sponsor-tile col-xs-12"/>
-                <Tile heading="Sponsor Name" desc="Sponsorship and support via" className="sponsor-tile col-xs-12"/>
-                <Tile heading="Sponsor Name" desc="Sponsorship and support via" className="sponsor-tile col-xs-12"/>
-            </div>
-        </section>
-
-        <section className="landing-section light-gray-background">
-        <div className="desktop-only">
-            <h2 className="light-gray-section-heading">Our Future</h2>
-        </div>
-        <div className="card desktop-only ">
-            <div className="container">
-            <div className="row">
-                <div className="col">
-                <p className="light-gray-card-text">Interested in contributing to OpenJ9? Check out the contributor guide in our Github repository.</p>
-                <Link to="#" className="btn btn-primary light-gray-section-button" role="button">Go to Docs</Link>
-                </div>
-                <div className="col">
-                <p className="light-gray-card-text">Interested in contributing to OpenJ9? Check out the contributor guide in our Github repository.</p>
-                <Link to="#" className="btn btn-primary light-gray-section-button" role="button">Go to  Blogs</Link>
-                </div>
-            </div>
-
-            </div>
-        </div>
-
-        <div className="container mobile-only">
-            <h2 className="light-gray-section-heading-mobile">Our Future</h2>
-            <div className="row">
-                <p className="light-gray-card-text">Interested in contributing to OpenJ9? Check out the contributor guide in our Github repository.</p>
-                <Link to="#" className="btn btn-primary light-gray-section-button" role="button">Go to Docs</Link>
-                <p className="light-gray-card-text">Interested in contributing to OpenJ9? Check out the contributor guide in our Github repository.</p>
-                <Link to="#" className="btn btn-primary light-gray-section-button" role="button">Go to Blogs</Link>
-            </div>
-        </div>
-
-        </section>
-
-    </Layout>
-)
-
-export default AboutPage
+export default () => (
+  <Layout isHome={false}>
+    <div sx={{ backgroundColor: "#F5F9FC", paddingX: "10%", paddingY: "5%" }}>
+      <Styled.h1>More About OpenJ9</Styled.h1>
+      <div
+        sx={{
+          backgroundColor: "white",
+          paddingY: 5,
+          paddingX: "5%",
+          borderRadius: "card"
+        }}
+      >
+        <Styled.h3>Our Story</Styled.h3>
+        <Styled.p>
+          This text should introduce the history of OpenJ9. It should discuss
+          how it started, why it started, and what its goals are. This paragraph
+          could recognize past achievements, new/existing initiatives and convey
+          the important role that Open Source plays in helping OpenJ9 achieve
+          its goals and empower its users. Etc, etc, etc….
+        </Styled.p>
+        <Button isLink={true} text="Call to Action" primary={true} />
+      </div>
+    </div>
+    <div
+      sx={{
+        backgroundColor: "primary",
+        color: "lightText",
+        paddingX: "10%",
+        paddingY: 5
+      }}
+    >
+      <Styled.h2>Our Sponsors</Styled.h2>
+      <Styled.p>
+        This text should reiterate the acheivements, goals, and continued work
+        underway to frame the reason for this section, namely to recognize that
+        none of this would be possible without the continued sponsorship and
+        support of OpenJ9’s partners.
+      </Styled.p>
+      <div
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          flexWrap: "wrap"
+        }}
+      >
+        <Card flex="0 0 400px" primary={false}>
+          <h1>Sponsor Example</h1>
+        </Card>
+        <Card flex="0 0 400px" primary={false}>
+          <h1>Sponsor Example</h1>
+        </Card>
+        <Card flex="0 0 400px" primary={false}>
+          <h1>Sponsor Example</h1>
+        </Card>
+      </div>
+    </div>
+    <div
+      sx={{
+        backgroundImage: `url(${circlePatternBig})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        paddingY: 5,
+        paddingX: "10%",
+        height: "50vh",
+        color: "white",
+        display: ["none", "none", "block", "block"]
+      }}
+    >
+      <div sx={{ marginTop: "5rem", marginLeft: "2rem", width: "40%" }}>
+        <Styled.h2>Our Future</Styled.h2>
+        <Styled.p sx={{ fontSize: 0 }}>
+          This section could be used as a way to communicate to potential
+          contributors about the OpenJ9 project, and the important role that
+          open source contributors play in helping to make OpenJ9 a resource
+          that developers not only use, but contriibute to as well. The last
+          line of this paragraph can be an invitation to check out whatever docs
+          or content that would help them learn more about Contributor
+          Guidelines for OpenJ9.
+        </Styled.p>
+      </div>
+    </div>
+    <div
+      sx={{
+        paddingX: "10%",
+        paddingY: 5,
+        backgroundColor: "#F5F9FC"
+      }}
+    >
+      <div
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+          backgroundColor: "white",
+          paddingX: "5%",
+          paddingY: "5%",
+          borderRadius: "card"        
+        }}
+      >
+        <article
+          sx={{
+            flex: "30%",
+            maxWidth: "600px",
+            minWidth: ["300px", "350px", "400px", "500px"]
+          }}
+        >
+          <Styled.h3>DOCS</Styled.h3>
+          <Styled.p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </Styled.p>
+          <Button isLink={true} text="Go To Docs" primary={true} />
+        </article>
+        <article
+          sx={{
+            flex: "30%",
+            maxWidth: "600px",
+            minWidth: ["300px", "350px", "400px", "500px"]
+          }}
+        >
+          <Styled.h3>BLOGS</Styled.h3>
+          <Styled.p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </Styled.p>
+          <Button isLink={true} text="Go To Blogs" primary={true} />
+        </article>
+      </div>
+    </div>
+  </Layout>
+);
