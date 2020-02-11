320a69e52b1ee94de78790fa163333535dd86e4d
Bazif Khan <bazif.khan@ibm.com>
2020-02-11 11:51:21 +0000

    Mobile View Navbar
    Signed-off-by: Bazif Khan <bazif.khan@ibm.com>

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,7 @@ import { ThemeProvider } from "theme-ui";
 import theme from "../theme";
 import Header from "./header";
 import Footer from "./footer";
+import MobileNav from "./mobileNav";
 import { Global } from "@emotion/core";
 
 export default ({ children, isHome }) => (
@@ -12,8 +13,12 @@ export default ({ children, isHome }) => (
         "*": { padding: 0, margin: 0 }
       })}
     />
+     
     <Header isHome={isHome} />
+    <MobileNav />
     {children}
+   
     <Footer />
+    
   </ThemeProvider>
 );
