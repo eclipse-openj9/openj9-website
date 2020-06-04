007ff5bb41eb924fb979b9cfd87ba9caa1a0030a
Bazif Khan <bazif.khan@ibm.com>
2020-06-04 08:59:58 +0100

    Google Analytics Enabled
    Signed-off-by: Bazif Khan <bazif.khan@ibm.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,17 +74,17 @@ module.exports = {
         path: `${__dirname}//news-page-content`,
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     // The property ID; the tracking code won't be generated without it
-    //     trackingId: "UA-105616558-3",
-    //     // Defines where to place the tracking script - `true` in the head and `false` in the body
-    //     head: false,
-    //     // Setting this parameter is optional
-    //     anonymize: true,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-105616558-3",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+      },
+    },
     {
       resolve: `gatsby-transformer-remark`,
       options: {
